f97e7f9395cc552ed1c3ae7d2825e6ad3ea0f156
sayanjit082805 <sayanjitukil2005@gmail.com>
2024-12-16 22:48:18 +0530

    add content

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Hydro Rangers</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/sayanjit082805/hydro-rangers-documentation',
   },
   chat: {
     link: 'https://discord.com',
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Powered By Next.js',
   },
 }
 
